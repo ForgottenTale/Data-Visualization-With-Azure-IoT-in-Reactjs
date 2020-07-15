1f6575040b941944c9534a35147da69cbf01ce51
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-07-15 17:30:34 +0530

    new design for the webapp

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,18 @@
-  import React,{useState, useEffect} from 'react';
- 
-  import {
-    Line,LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-    ResponsiveContainer
-  } from 'recharts';
-  import io from 'socket.io-client';
-  const socket = io('http://localhost:5000');
+import React from 'react';
+import './App.css';
+import Nav from './components/nav';
 
-  function App() {
-  
-    const [dataReact, setData] = useState([]);
+import Home from './components/home';
 
-
-
-    useEffect(() => {
-      socket.on('data1', res => {
-        console.log(res);
-      setData(currentData => [...currentData, res]);
+function App() {
+  return (
+    <div className="App">
+        <Nav/>
      
-      
-      });
-  }, []);
+        <Home />
 
-  if(dataReact.length > 10)
-  {
-    dataReact.shift();
-  }
+    </div>
+  );
+}
 
-    return (
-      <div className="App">
-        <ResponsiveContainer width="99%" aspect={3} >
-        <LineChart
-        width={500}
-        height={300}
-        data={dataReact}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="MessageDate" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
-      </LineChart>
-        </ResponsiveContainer>
-        
-        
-      </div>
-    );
-  }
-
-  export default App;
+export default App;
--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
-const router =require('./router')
+const router = require('./router')
 const { EventHubProducerClient, EventHubConsumerClient } = require('@azure/event-hubs');
 const { convertIotHubToEventHubsConnectionString } = require('./iot-hub-connection-string.js')
 var consumerClient;
 
 // Enter your iothub connection string in .evc file
 
-const iotHubConnectionString = "HostName=Nodehube.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+ctwhNAt7hVf6lGKRE6Uv7miAjZy7cV4hF5bx6mHK7s=";
+const iotHubConnectionString = "HostName=NodeMCUHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GQjC3mv6S9uYMPCHE4B+yfQWkspE1c+eYDOFqhGzFlA=";
 const eventHubConsumerGroup = "node";
 
 // Creating the server
@@ -23,18 +23,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 app.use(router);
 app.use(cors());
-server.listen(PORT, ()=>console.log('server is running on port' ,PORT));
+server.listen(PORT, () => console.log('server is running on port', PORT));
 
 // Function to start the connection to iothub
 
-async function startConnection(){
+async function startConnection() {
   try {
-  const eventHubConnectionString = await convertIotHubToEventHubsConnectionString(iotHubConnectionString);
-  consumerClient = new EventHubConsumerClient(eventHubConsumerGroup, eventHubConnectionString);
-  console.log('Successfully created the EventHubConsumerClient from IoT Hub event hub-compatible connection string.');
-  const partitionIds = await consumerClient.getPartitionIds();
-  console.log('The partition ids are: ', partitionIds);
-  }catch (ex) {
+    const eventHubConnectionString = await convertIotHubToEventHubsConnectionString(iotHubConnectionString);
+    consumerClient = new EventHubConsumerClient(eventHubConsumerGroup, eventHubConnectionString);
+    console.log('Successfully created the EventHubConsumerClient from IoT Hub event hub-compatible connection string.');
+    const partitionIds = await consumerClient.getPartitionIds();
+    console.log('The partition ids are: ', partitionIds);
+  } catch (ex) {
     console.error(ex.message || ex);
   }
 
@@ -66,33 +66,46 @@ async function startReadMessage(startReadMessageCallback) {
 
 // Socket.io part
 
-  io.sockets.on('connection', (socket) => {
+io.sockets.on('connection', (socket) => {
 
-    
-    console.log(`new connection id: ${socket.id}`);
-  
-  
+
+  console.log(`new connection id: ${socket.id}`);
+
+
   (async () => {
     await startReadMessage((message, date, deviceId) => {
       try {
-        var time = date.toISOString().slice(11,18);
-       let payload = {
-        temperature: message.temperature.toFixed(2),
-        Humidity: message.humidity.toFixed(2),
-        MessageDate: time || Date.now().toISOString(),
-        DeviceId: deviceId,
-      };
+
+        const options = {
+          timeZone: "Asia/Kolkata",
+          hour12: false,
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit"
+        }
+        var time = new Date().toLocaleTimeString("en-IN", options).slice(0, 8);
+        var date = date.toISOString().slice(0, 10);
+        var dateTime = [date, time].filter(Boolean).join(" ");
+
+  
+        let payload = {
+          Pressure: message.Pressure.toFixed(2),
+          Ph: message.Ph.toFixed(2),
+          Flowrate: message.Flowrate.toFixed(2),
+          date: dateTime,
+          DeviceId: message.deviceId,
+        };
         console.log(payload);
-        socket.broadcast.emit('data1',payload);   
-     
+        socket.broadcast.emit('data1', payload);
+
       } catch (err) {
         console.error('Error broadcasting: [%s] from [%s].', err, message);
       }
     });
   })().catch();
-    socket.on('disconnect',()=>{
-   
-        console.log('Disconneted');
-    })
+  socket.on('disconnect', () => {
+
+    console.log('Disconneted');
   })
+})
 
